27536e24e7ef29775e37f73bda8f295fcbe20489
Gaston Nicolás Díaz <gastondiazn@gmail.com>
2023-09-18 10:35:15 -0300

    Agrega estructura switch

---
+++ app.js
@@ -0,0 +1,20 @@
+// todo: hacer el front de la calculadora
+const process =require ('process')
+const {argv} =process 
+const comando= argv[2]
+function calculadora(comando){
+switch (comando.toLowerCase()) {
+    case "sumar":
+        
+        break;
+    case "restar":
+        break
+    case "multiplicar":
+        break
+    case "dividir":
+        break
+    default:
+        break;
+}
+}
+console.log(calculadora);
