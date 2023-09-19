12c7e632bc53d460605ae93cc3997deb2df8046b
Gaston Nicolás Díaz <gastondiazn@gmail.com>
2023-09-19 09:59:03 -0300

    fix

--- app.js
+++ app.js
@@ -2,17 +2,19 @@
 const process =require ('process')
 const resta=require('./restar')
 const suma= require('./sumar')
+const dividir= require('./dividir')
+const multiplicar= require('./multiplicar')
 const {argv} =process 
 function calculadora(comando,a,b){
 switch (comando.toLowerCase()) {
     case "sumar":
-        suma(a,b)
+        return suma(a,b)
         break;
     case "restar":
         return resta(a,b)
         break
     case "multiplicar":
-        multiplicar(a,b)
+        return multiplicar(a,b)
         break
     case "dividir":
         return dividir(a,b)
